aba9c3822334c124b66b2c36bcede0d7eca0a604
inhohwangg <sk1440sk@naver.com>
2023-02-06 23:18:52 +0900

    app.js app.post add

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = require('express')();
 const connect = require('./schema/index');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const Post = require('../schema/post');
 
 connect();
 
 // 라우터 불러오기
 const postRouter = require('./routes/post');
+const post = require('./schema/post');
 
 // 각종 미들웨어
 app.use(helmet());
@@ -21,6 +23,17 @@ app.use('/api', [postRouter]);
 
 app.get('/', (req, res) => {
   res.send('잘 작동하는듯');
+});
+
+app.post('/test', async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    await Post.create({ title, content });
+    res.status(201).json({ title, content });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ result: false });
+  }
 });
 
 app.listen(3000, () => {
