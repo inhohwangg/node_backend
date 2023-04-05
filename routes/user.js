2147b01559ef47cbf1d041e5eef3b3f1ef62bcbb
inhohwangg <sk1440sk@naver.com>
2023-04-05 22:32:52 +0900

    로그인,회원가입 bcrypt 해싱 추가,입력값 유효 검증 추가

--- routes/user.js
+++ routes/user.js
@@ -2,19 +2,36 @@ const express = require('express');
 const router = express.Router();
 const User = require('../schema/user');
 const jwt = require('jsonwebtoken');
-// const secretKey = process.env.key;
+const bcrypt = require('bcrypt');
+// const snowflake = require('node-snowflake').Snowflake;
 require('dotenv').config();
 
 //! 회원가입 API
 router.post('/signup', async (req, res) => {
   try {
     const { userId, password, name } = req.body;
-    const user = new User({ userId, password, name });
+
+    // 입력값 유효성 검사
+    if (!userId || !password || !name) {
+      return res.status(400).json({ error: 'Invalid input' });
+    }
+
+    // userId 생성
+    // const snowflakeId = snowflake.nextId().toString();
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // 유저 정보 저장
+    const user = new User({
+      userId: userId,
+      password: hashedPassword,
+      name,
+    });
     await user.save();
 
-    const token = jwt.sign({ userId: user._id });
+    // 토큰 생성
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
 
-    res.json({ token });
+    res.json({ userId, password, token });
   } catch (e) {
     console.log(e);
     res.status(400).json({ result: false });
@@ -25,30 +42,29 @@ router.post('/signup', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     const { userId, password } = req.body;
-
+    console.log(userId, password);
+    // 입력받은 userId를 이용하여 User 모델에서 해당 유저 정보를 찾아냅니다.
     const user = await User.findOne({ userId });
     console.log(user);
-
+    // 해당 유저 정보가 없으면 에러를 반환합니다.
     if (!user) return res.status(401).json({ error: 'Invalid credentials' });
-    // const isMatch = await user.comparePassword(password);
 
-    // if (!isMatch) return res.status(401).json({ error: 'Invalid credentials' });
-    // console.log('this?');
-    // const token = jwt.sign({ userId: user.userId }, secretKey);
-    // console.log('this????');
-    if (
-      userId == '' ||
-      userId == null ||
-      userId == undefined ||
-      password == '' ||
-      password == null ||
-      password == undefined
-    )
-      throw Error;
-    res.status(200).json({ userId, password });
+    // 입력받은 password와 해당 유저의 비밀번호를 비교합니다.
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    // 비밀번호가 일치하지 않으면 에러를 반환합니다.
+    if (!isMatch) return res.status(401).json({ error: 'Invalid credentials' });
+
+    // JWT 토큰을 생성합니다.
+    const token = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    // 유저 정보와 토큰을 함께 반환합니다.
+    res.status(200).json({ user, token });
   } catch (e) {
     console.log(e);
-    res.status(400).json({ result: false });
+    res.status(500).json({ error: 'Server Error' });
   }
 });
 
